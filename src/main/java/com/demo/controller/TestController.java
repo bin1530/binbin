c9f2f3c0f9ab64e5a381dc9e0a74847e3f3e8ad1
bin <1611021382@qq.com>
2021-09-14 16:46:16 +0800

    second commit

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
-
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+    }
     @GetMapping("hello")
     public String hello(){
         return "hello我";
